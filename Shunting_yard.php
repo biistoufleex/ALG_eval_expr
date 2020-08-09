ef9e700a67b4f4e2856cc266452ed5565733964c
kevin <kevin.aubel@epitech.eu>
2020-08-09 13:16:40 +0200

    Correction tout est ok maintenant

--- Shunting_yard.php
+++ Shunting_yard.php
@@ -1,6 +1,6 @@
 <?php
 
-class Shunting_yard
+class Eval_expr
 {
     public $expr  = [];
     public $stack = [];
@@ -11,6 +11,7 @@ class Shunting_yard
         '-' => 1,
         '*' => 2,
         '/' => 2,
+        '%' => 2,
     ];
     public $left   = 0;
     public $right  = 0;
@@ -18,7 +19,10 @@ class Shunting_yard
 
     function __construct($expr)
     {
-        $this->expr = str_split($expr);
+
+        preg_match_all('/(\(|\d*(?:\.\d+)?|-|\+|\/|\*|\%|\))/', $expr, $output);
+        $this->expr = $output[0];
+        print_r($this->expr);
         $this->createNpi();
         $this->solveNpi();
     }
@@ -27,30 +31,35 @@ class Shunting_yard
     {
         while (!empty($this->expr)) {
 
-            if (is_numeric($this->expr[0])) {
-                array_push($this->npi, array_shift($this->expr));
+            if ($this->expr[0] === "") {
+                array_shift($this->expr);
             } else {
 
-                if (empty($this->stack) || $this->expr[0] == "(") {
-                    array_push($this->stack, array_shift($this->expr));
+                if (is_numeric($this->expr[0])) {
+                    array_push($this->npi, array_shift($this->expr));
                 } else {
 
-                    if ($this->expr[0] === ")") {
-                        while (end($this->stack) != "(") {
-                            array_push($this->npi, array_pop($this->stack));
-                        }
-                        array_splice($this->expr, 0, 1);
-                        array_splice($this->stack, count($this->stack) - 1, 1);
+                    if (empty($this->stack) || $this->expr[0] == "(") {
+                        array_push($this->stack, array_shift($this->expr));
                     } else {
 
-                        if ($this->value[$this->expr[0]] > $this->value[end($this->stack)]) {
-                            array_push($this->stack, array_shift($this->expr));
+                        if ($this->expr[0] === ")") {
+                            while (end($this->stack) != "(") {
+                                array_push($this->npi, array_pop($this->stack));
+                            }
+                            array_splice($this->expr, 0, 1);
+                            array_splice($this->stack, count($this->stack) - 1, 1);
                         } else {
 
-                            while ($this->value[end($this->stack)] >= $this->value[$this->expr[0]]) {
-                                array_push($this->npi, array_pop($this->stack));
-                                if (empty($this->stack)) {
-                                    break;
+                            if ($this->value[$this->expr[0]] > $this->value[end($this->stack)]) {
+                                array_push($this->stack, array_shift($this->expr));
+                            } else {
+
+                                while ($this->value[end($this->stack)] >= $this->value[$this->expr[0]]) {
+                                    array_push($this->npi, array_pop($this->stack));
+                                    if (empty($this->stack)) {
+                                        break;
+                                    }
                                 }
                             }
                         }
@@ -66,6 +75,7 @@ class Shunting_yard
     function solveNpi()
     {
         $this->expr = $this->npi;
+        echo implode($this->expr);
         $this->npi = [];
 
         while (!empty($this->expr)) {
@@ -73,7 +83,7 @@ class Shunting_yard
             if (is_numeric($this->expr[0])) {
                 array_push($this->stack, array_shift($this->expr));
             } else {
-                
+
                 $this->right = array_pop($this->stack);
                 $this->left = array_pop($this->stack);
 
@@ -94,6 +104,10 @@ class Shunting_yard
                         $this->result = $this->left / $this->right;
                         break;
 
+                    case '%':
+                        $this->result = $this->left % $this->right;
+                        break;
+
                     default:
                         break;
                 }
@@ -106,5 +120,4 @@ class Shunting_yard
     }
 }
 
-// $alg = new Shunting_yard('((3*5-4*7)/1+1)-1/8*8+3');
-// $alg = new Shunting_yard('-1/8*8+3');
+// $alg = new Eval_expr('((3*5-4*7)/1+1)-1/8*8+3');
